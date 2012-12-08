cf5613ecd6afa76926d2d90cf1a043d628b6115e
Jessica Allen <jessicaspacekat+github@gmail.com>
2012-12-07 17:54:36 -0800

    Starts with a random first quote. Less/More controls.
    Still no tests :(

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,26 @@
 
 /* Controllers */
 function IpsumCtrl($scope, $http) {
+  $scope.quoteCount = 1;
   $scope.ipsum = ""
   $scope.getIpsum = function() {
     $http.get("/app/ipsum/words.txt").success(function(data) {
-      $scope.ipsum = data.split("\n");
+      $scope.ipsum = shuffle(data.split("\n"));
     })
+  }
+  function shuffle(data) {
+    for(var j, x, i = data.length; i; j = parseInt(Math.random() * i), x = data[--i], data[i] = data[j], data[j] = x);
+    return data;
+  }
+  $scope.tooFew = function() {
+    if ($scope.quoteCount < 2) return true;
+    else return false;
+  }
+  $scope.getIpsum();
+  $scope.addIpsum = function() {
+    $scope.quoteCount = $scope.quoteCount + 1;
+  }
+  $scope.lessIpsum = function() {
+    $scope.quoteCount = $scope.quoteCount - 1;
   }
 }
--- app/js/filters.js
+++ app/js/filters.js
@@ -3,16 +3,9 @@
 /* Filters */
 
 angular.module('myApp.filters', []).
-  filter('shuffle', [function() {
-    return function(data) {
-      for(var j, x, i = data.length; i; j = parseInt(Math.random() * i), x = data[--i], data[i] = data[j], data[j] = x);
-      return data;
-    }
-  }]).
   filter('truncate', [function() {
-    // "characters" are actually "quotes" now. need to rename.
-    return function(data, characters) {
-      return data.slice(0, characters);
+    return function(data, quoteCount) {
+      return data.slice(0, quoteCount);
     }
   }]).
   filter('join', [function() {
