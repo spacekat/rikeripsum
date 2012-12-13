b78daa8be3d20e9c8a8651a05d2afd685f6b801b
Jessica Allen <jessicaspacekat+github@gmail.com>
2012-12-13 10:32:45 -0800

    div instead of textarea and one-click select.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -25,3 +25,15 @@ function IpsumCtrl($scope, $http) {
     $scope.quoteCount = $scope.quoteCount - 1;
   }
 }
+
+function selectText(containerid) {
+  if (document.selection) {
+    var range = document.body.createTextRange();
+    range.moveToElementText(document.getElementById(containerid));
+    range.select();
+  } else if (window.getSelection) {
+    var range = document.createRange();
+    range.selectNode(document.getElementById(containerid));
+    window.getSelection().addRange(range);
+  }
+}
