3270b1af06dca2ec225afc73f2303efc42978436
Ian Duffy <ian@ianduffy.ie>
2013-01-04 01:12:11 +0000

    switch to hashbang in urls... use html5 pushstate urls in supported browsers

--- app/js/app.js
+++ app/js/app.js
@@ -3,7 +3,9 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
-  config(['$routeProvider', function($routeProvider) {
+  config(['$routeProvider','$locationProvider', function($routeProvider,locationProvider) {
+    $locationProvider.html5Mode(false);
+    $locationProvider.hashPrefix('!');
     $routeProvider.when('/ipsum', {templateUrl: 'partials/ipsum.html', controller: IpsumCtrl});
     $routeProvider.otherwise({redirectTo: '/ipsum'});
   }]);
