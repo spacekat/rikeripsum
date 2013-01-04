31e6ef508c89f286f553f341bc809e1bf84943c1
Ian Duffy <ian@ianduffy.ie>
2013-01-04 01:28:30 +0000

    General clean up of the javascript

--- app/js/app.js
+++ app/js/app.js
@@ -2,8 +2,8 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
-  config(['$routeProvider','$locationProvider', function($routeProvider,locationProvider) {
+var myApp = angular.module('myApp', [])
+  .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(false);
     $locationProvider.hashPrefix('!');
     $routeProvider
