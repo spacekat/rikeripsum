b7d2a13aceae5e5423ae81b6243b7e9a64b9aacc
Ian Duffy <ian@ianduffy.ie>
2013-01-04 01:12:58 +0000

    remove ipsum from urls

--- app/js/app.js
+++ app/js/app.js
@@ -6,6 +6,12 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives'])
   config(['$routeProvider','$locationProvider', function($routeProvider,locationProvider) {
     $locationProvider.html5Mode(false);
     $locationProvider.hashPrefix('!');
-    $routeProvider.when('/ipsum', {templateUrl: 'partials/ipsum.html', controller: IpsumCtrl});
-    $routeProvider.otherwise({redirectTo: '/ipsum'});
+    $routeProvider
+      .when('/', {
+        templateUrl: 'partials/ipsum.html',
+        controller: 'IpsumCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
   }]);
